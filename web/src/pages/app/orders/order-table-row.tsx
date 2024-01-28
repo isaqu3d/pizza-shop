1339700a150778132aacadf4302b2ebb340feb3e
Isaque de Sousa <isaque.error404@gmail.com>
2024-01-28 11:29:02 -0300

    feat: add order details

--- web/src/pages/app/orders/order-table-row.tsx
+++ web/src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,24 @@
+import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { ArrowRight, Search, X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { TableCell, TableRow } from "@/components/ui/table";
+
+import { OrderDetails } from "./order-details";
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <p className="sr-only">Detalhes do pedido</p>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <p className="sr-only">Detalhes do pedido</p>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         f789dsa0432fd
