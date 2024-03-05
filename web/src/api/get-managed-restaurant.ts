ab2b161fdc140dfc3f38ca39f85ee9b570dac5e0
Isaque de Sousa <isaque.error404@gmail.com>
2024-03-05 19:27:48 -0300

    feat:(api): export type

--- web/src/api/get-managed-restaurant.ts
+++ web/src/api/get-managed-restaurant.ts
@@ -1,6 +1,6 @@
 import { api } from "@/lib/axios";
 
-interface GetManagerRestaurantResponse {
+export interface GetManagerRestaurantResponse {
   id: string;
   name: string;
   description: string | null;
