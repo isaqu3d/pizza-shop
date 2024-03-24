9865f1399840ea1dd3afc12bb0b34be60621fc36
Isaque de Sousa <isaque.error404@gmail.com>
2024-03-24 12:00:24 -0300

    feat(receipt): add loading skeleton

--- web/src/pages/app/dashboard/month-orders-amount-card.tsx
+++ web/src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -4,6 +4,8 @@ import { Utensils } from "lucide-react";
 import { getMonthOrdersAmount } from "@/api/dashboard/get-month-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
+import { MetricCardSkeleton } from "./metric-card-skeleton";
+
 export function MonthOrdersAmountCard() {
   const { data: monthOrdersAmount } = useQuery({
     queryFn: getMonthOrdersAmount,
@@ -18,7 +20,7 @@ export function MonthOrdersAmountCard() {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        {monthOrdersAmount && (
+        {monthOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {monthOrdersAmount.amount.toLocaleString("pt-BR")}
@@ -36,6 +38,8 @@ export function MonthOrdersAmountCard() {
               em relação ao mês passado
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
