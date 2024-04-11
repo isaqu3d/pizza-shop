85aae488c8abc9957d170da15c5405be01d76961
Isaque de Sousa <isaque.error404@gmail.com>
2024-04-11 16:20:22 -0300

    chore: rename function

--- server/src/db/schema/auth-links.ts
+++ server/src/db/schema/auth-links.ts
@@ -2,7 +2,7 @@ import { createId } from "@paralleldrive/cuid2";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { users } from ".";
 
-export const authLink = pgTable("auth_links", {
+export const authLinks = pgTable("auth_links", {
   id: text("id")
     .$defaultFn(() => createId())
     .primaryKey(),
