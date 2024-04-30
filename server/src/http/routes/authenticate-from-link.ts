17caec329cc11cd9789ff30298e3fb2aa81c852d
Isaque de Sousa <isaque.error404@gmail.com>
2024-04-29 23:46:19 -0300

    refactor(route): add derive

--- server/src/http/routes/authenticate-from-link.ts
+++ server/src/http/routes/authenticate-from-link.ts
@@ -7,7 +7,7 @@ import { auth } from "../auth";
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   "/auth-links/authenticate",
-  async ({ query, jwt, set, cookie: { auth } }) => {
+  async ({ query, signUser, set }) => {
     const { code, redirect } = query;
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -35,15 +35,10 @@ export const authenticateFromLink = new Elysia().use(auth).get(
       },
     });
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
     });
-
-    auth.httpOnly = true;
-    auth.value = token;
-    auth.maxAge = 60 * 60 * 24 * 7; // 7 days
-    auth.path = "/";
 
     await db.delete(authLinks).where(eq(authLinks.code, code));
     set.redirect = redirect;
