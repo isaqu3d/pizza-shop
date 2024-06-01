c707e1be61e1eac86060d439f105a6be93e5e2be
Isaque de Sousa <isaque.error404@gmail.com>
2024-06-01 00:33:18 -0300

    feat(server): add cors and validation

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,3 +1,4 @@
+import cors from "@elysiajs/cors";
 import { Elysia } from "elysia";
 import { authenticateFromLink } from "./routes/authenticate-from-link";
 import { getManagedRestaurant } from "./routes/get-managed-restaurant";
@@ -7,12 +8,42 @@ import { sendAuthLink } from "./routes/send-auth-link";
 import { signOut } from "./routes/sign-out";
 
 const app = new Elysia()
+  .use(
+    cors({
+      credentials: true,
+      allowedHeaders: ["content-type"],
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+      origin: (request): boolean => {
+        const origin = request.headers.get("origin");
+
+        if (!origin) {
+          return false;
+        }
+
+        return true;
+      },
+    })
+  )
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
-  .use(getManagedRestaurant);
+  .use(getManagedRestaurant)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case "VALIDATION": {
+        set.status = error.status;
+
+        return error.toResponse();
+      }
+      default: {
+        console.error(error);
+
+        return new Response(null, { status: 500 });
+      }
+    }
+  });
 
 app.listen(3333, () => {
   console.log("🔥 HTTP server running!");
