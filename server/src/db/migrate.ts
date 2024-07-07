8bbe251e630086a579bd082814232e1056e06002
Isaque de Sousa <isaque.error404@gmail.com>
2024-07-07 12:21:11 -0300

    refactor: refactor file migrate

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,18 +1,16 @@
+import chalk from "chalk";
+import postgres from "postgres";
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-import postgres from "postgres";
 import { env } from "../../env";
 
-import chalk from "chalk";
-
-const connection = postgres(env.DATABASE_URL, {
-  max: 1,
-});
-
+const connection = postgres(env.DATABASE_URL, { max: 1 });
 const db = drizzle(connection);
 
 await migrate(db, { migrationsFolder: "drizzle" });
-console.log(chalk.greenBright("Migrations applied successfully"));
+
+console.log(chalk.greenBright("Migrations applied successfully!"));
 
 await connection.end();
 
